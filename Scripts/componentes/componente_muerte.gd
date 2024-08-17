8d61efec9e79e032be307b7b0b1863d8a1f6800b
StyloKun <alancaro.2004@gmail.com>
2024-08-17 19:27:01 -0400

    character controller, falta pulir y arreglar hitboxs

---
+++ Scripts/componentes/componente_muerte.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@export var vida : Node2D
+var health = vida.vida_player
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+	if health<=0 :
+		queue_free()
+	
---
+++ Scripts/componentes/componente_vida.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+
+@export var vida_max:=10
+var vida_player:int
+
+func _ready() :
+	vida_player = vida_max
+	
+func restar_vida(atack):
+	vida_player -= atack
+
+	
+func sumar_vida(curas):
+	vida_player += curas
+	
